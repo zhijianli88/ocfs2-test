2bc0d887a54129124ddcee693de1e16794ef3a52
Junxiao Bi <junxiao.bi@oracle.com>
2017-02-17 16:33:26 +0800

    single: splice: fix read failure
    Splice read at most pipe buffer size one time, this will cause partial
    read if file size over pipe buffer size.
    
    Signed-off-by: Junxiao Bi <junxiao.bi@oracle.com>

--- programs/splice/splice_read.c
+++ programs/splice/splice_read.c
@@ -7,12 +7,15 @@
 #include <assert.h>
 #include <errno.h>
 #include <limits.h>
+#include <sys/stat.h>
 
 int main(int argc, char *argv[])
 {
 	int fd;
 	int slen;
-	
+	struct stat st;
+	int size;
+
 	if (argc < 2) {
 		printf("Usage: ./splice_read out | cat\n");
 		exit(-1);
@@ -23,14 +26,23 @@ int main(int argc, char *argv[])
 		printf("open file failed.\n");
 		exit(-1);
 	}
-	slen = splice(fd, NULL, STDOUT_FILENO, NULL, 10000000, 0);
-	if (slen < 0)
-		fprintf(stderr, "splice failed.\n");
-	else
-		fprintf(stderr, "spliced length = %d\n",slen);
+	stat(argv[1], &st);
+	size = st.st_size;
+
+	while (size > 0) {
+		slen = splice(fd, NULL, STDOUT_FILENO, NULL, size, 0);
+		if (slen < 0) {
+			fprintf(stderr, "splice failed.\n");
+			break;
+		} else {
+			fprintf(stderr, "spliced length = %d\n",slen);
+			size -= slen;
+		}
+	}
 	close(fd);
 	if (slen <0)
 		exit(-1);
+
 	return 0;
 }
 
